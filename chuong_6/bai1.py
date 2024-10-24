7b84cdcd05ad67542e538df6220e1e13c1b50245
trinhquocnhat <tqnhat.24174600070@sv.uneti.edu.vn>
2024-10-24 20:34:02 +0700

    Add files via upload

--- chuong_6/bai1.py
+++ chuong_6/bai1.py
@@ -1,1 +1,14 @@
-
+#bai 1
+import math
+    # Nhập bán kính và chiều cao từ bàn phím
+r  = float(input("Nhập bán kính (r): "))
+h  = float(input("Nhập chiều cao (h): "))
+pi = 3.14
+       #Tính diện tích xung quanh, diện tích toàn phần và thể tích khối trụ
+S_xung_quanh = 2 * math.pi * r * h
+S_toan_phan = 2 * math.pi * r * h + 2 * pi * r**2
+the_tich = pi * r**2 * h
+# Kết quả
+print(f"Diện tích xung quanh: {round(S_xung_quanh, 2)} ")
+print(f"Diện tích toàn phần: {round(S_toan_phan, 2)} ")
+print(f"Thể tích: {round(the_tich, 2)}")
